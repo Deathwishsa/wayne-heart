8cb421141aa86074f35b5b60a243509ba433df13
Jarryd <jarrydleroux94@gmail.com>
2026-05-22 01:00:17 +0200

    remove gallery

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './page/home/home.component';
 import { ContactUsComponent } from './page/contact-us/contact-us.component';
-import { GalleryComponent } from './page/gallery/gallery.component';
+// import { GalleryComponent } from './page/gallery/gallery.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'gallery', component: GalleryComponent },
+  // { path: 'gallery', component: GalleryComponent },
   { path: 'contact-us', component: ContactUsComponent },
   { path: '**', redirectTo: '' }
 ];
